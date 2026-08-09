25c06746df78308e118a45aa75e983a81c13ad58
Nazim Saifi <saifinazim2311@gmail.com>
2026-08-09 21:27:17 +0530

    http-backend api almost done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,59 +1,158 @@
 import express from 'express'
 import auth from './middleware/auth.js';
-import {UserSignInSchema, UserSignUpSchema} from "@repo/common/types"
-
-
+import {UserSignInSchema, UserSignUpSchema,SECRET_KEY} from "@repo/common"
+import { Request } from 'express';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import {prisma} from "@repo/db/prisma"
-
+export interface RequestHandler extends Request {
+  userId?: string;
+}
 const PORT=3001
 
 const app = express();
 
 
 app.use(express.json())
+app.use(cookieParser())
 
 
 
 app.post('/signup',async(req,res)=>{
      const { username, password,email } = req.body;
-    const data=await prisma.user.create({
-      data:{
-        email,
-        username,
-        password
+     if(!username || !password || !email){
+       return res.status(401).json({
+         success: false,
+         message: "Invalid credentials",
+       });
+     }
+      if (!UserSignUpSchema.safeParse({ username, password, email }).success) {
+        return res.status(401).json({
+          success:false,
+          message: "Invalid credentials",
+        });
+      }
+
+      const isExist=await prisma.user.findFirst({where:email});
+      if(isExist){
+         return res.status(200).json({
+           success: false,
+           message: "User already exist"
+         });
+      }
+      let hash_pwd="";
+      try {
+        hash_pwd = await bcrypt.hash(password, 10);
+
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+          success:false,
+          message:"error while hashing pwd"
+        })
+      }
+    try {
+      const user = await prisma.user.create({
+        data: {
+          email,
+          username,
+          password: hash_pwd,
+        },
+        select:{
+          id:true,
+          email:true,
+          username:true
+        }
+      });
+      const payload = {
+    userId:user.id
+};
+      const token=jwt.sign(payload,SECRET_KEY,{
+        expiresIn:'7d'
+      })
+      
+      return res.cookie('token',token,{
+        httpOnly:true,
+        maxAge:24*60*60*60*1000
+      }).status(200).json({
+        success: true,
+        data:user,
+        token
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "error while putting entry in Database",
+      });
+    }
+})
+app.post('/signin',async(req,res)=>{
+   
+     const { email, password } = req.body;
+if ( !password || !email) {
+  return res.status(401).json({
+    success: false,
+    message: "Invalid credentials",
+  });
+}
+     if (!UserSignInSchema.safeParse({ email, password}).success) {
+       return res.status(401).json({
+         success:false,
+         message: "Invalid credentials",
+       });
+     }
+    const user = await prisma.user.findFirst({
+      where:{
+        email
       }
     })
-     if (!UserSignUpSchema.safeParse({ username, password,email })) {
-       return res.json({
-         message: "Invalid credentials",
-       });
-     }
-     
-    return res.json({
-        user:data
-    })
-})
-app.post('/signin',(req,res)=>{
-   
-     const { username, password } = req.body;
-
-     if (!UserSignInSchema.safeParse({ username, password})) {
-       return res.json({
-         message: "Invalid credentials",
-       });
-     }
-    const userid=123;
     // db-call
-    return res.json({
-        message:"user logged in"
-    })
+    if(!user){
+       return res.status(401).json({
+         success: false,
+         message:"Invalid credentials"
+       });
+    }
+    const payload = {
+      userId: user.id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        success: true,
+        data: user,
+        token,
+      });
 })
 
-app.post('/create-room',auth,(req,res)=>{
-    // db-call
+app.post('/create-room',auth,(req:RequestHandler,res)=>{
+    
+    const userId=req.userId
+    const { roomName}=req.body
+    // zod test
+    // await prisma.room.create({data:{userId,slug:roomName}})
+    
     return res.json({
         message:"room created successfully!!"
     })
+})
+
+app.get('/allRooms',auth,(req,res)=>{
+      // show room that user has joined
+      // show all the rooms
+      // return await prisma.room.findMany({});
+      //( optional ) add pagination for rooms
+      
 })
 app.get('/',(req,res)=>{
     res.send("hello world")
--- apps/http-backend/src/middleware/auth.ts
+++ apps/http-backend/src/middleware/auth.ts
@@ -1,19 +1,34 @@
+import { SECRET_KEY } from "@repo/common";
 import { NextFunction, Request, Response } from "express"
+import jwt from 'jsonwebtoken'
+import { RequestHandler } from "../index";
 
 
-
-const auth=async(req:Request,res:Response,next:NextFunction)=>{
+const auth=async(req:RequestHandler,res:Response,next:NextFunction)=>{
 
         try {
             const token=req.cookies.token
-            if(!token) return res.json({message:"failed"})
+            if(!token) return res.json({
+              success: false,
+              message: "failed",
+            });
             
             // verify via jwt
-            // decode the token
+            const decode=jwt.verify(token,SECRET_KEY) as {userId:string};
+             if (!decode || !decode.userId)
+               return res.json({
+                 success: false,
+                 message: "Token is not valid",
+               });
+           req.userId=decode.userId
             next()
 
         } catch (error) {
             console.log(error)
+            return res.status(500).json({
+                success:false,
+                message:"System Error while authorizing user"
+            })
         }
 
 
